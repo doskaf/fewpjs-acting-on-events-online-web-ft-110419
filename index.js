1aec8086af63b0ad3c9508de627af701fcc0444b
Dominique Skaf <doskaf@yahoo.com>
2020-02-21 16:26:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ function moveDodgerLeft() {
   }
 }
 
-function moveDodgerLeft() {
-  let leftNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
+function moveDodgerRight() {
+  let rightNumbers = dodger.style.right.replace("px", "");
+  let right = parseInt(rightNumbers, 10);
  
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
+  if (right < 360) {
+    dodger.style.right = `${right + 1}px`;
   }
 }
 
