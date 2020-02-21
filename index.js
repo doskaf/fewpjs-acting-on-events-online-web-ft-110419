fa254138889bdd9dc9cfbbfcfb2207a86ed85e07
Dominique Skaf <doskaf@yahoo.com>
2020-02-21 16:28:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ function moveDodgerRight() {
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
   }
 });
